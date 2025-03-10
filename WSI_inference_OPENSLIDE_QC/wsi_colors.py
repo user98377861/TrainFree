96c29ff29bf224bcc8b89d6a93d3dd4a94c17c78
user98377861 <18037080487@163.com>
2025-03-10 11:27:24 +0800

    update

---
+++ WSI_inference_OPENSLIDE_QC/wsi_colors.py
@@ -0,0 +1,11 @@
+colors_QC7 = [
+    [128, 128, 128],     #1. ART_NORM: white
+    [255, 99, 71],     #2. ART_FOLD: orange
+    [0, 255, 0],   #3. ART_DARKSPOT: green
+    [255, 0, 0],   #4. ART_PEN: red
+    [255, 0, 255],   #5. ART_EDGE: pink
+    [75, 0, 130],      #6. ART_FOCUS: violet
+    [255, 255, 255]   #7. BACKGROUND
+]
+ 
+ 
